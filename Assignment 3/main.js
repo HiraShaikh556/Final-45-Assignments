d330c845a2ff7f4d31103267c164254e762caea4
[hira  shaikh] <[hirasheikh556@gmail.com]>
2024-06-22 15:25:38 +0500

    first commit

---
+++ Assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Storing the famous quote and its author
+let quote = "Nothing is impossible because impossible itself I M POSSIBLE";
+let author = "Einstein";
+//Printing the message
+console.log(`"${quote}"-${author}`);
---
+++ Assignment 3/main.ts
@@ -0,0 +1,9 @@
+//Storing the famous quote and its author
+
+let quote: string ="Nothing is impossible because impossible itself I M POSSIBLE"
+
+let author: string = "Einstein"
+
+//Printing the message
+
+console.log(`"${quote}"-${author}`);
