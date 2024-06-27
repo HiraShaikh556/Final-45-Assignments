9df0bdf5c84a9c8b39c587aba46e17eb0b758d54
[hira  shaikh] <[hirasheikh556@gmail.com]>
2024-06-27 13:04:37 +0500

    first commit

---
+++ Assignment 7/main.ts
@@ -0,0 +1,15 @@
+//assignment no 07
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+// Be sure to enclose your operations in print statements to see the results.
+
+let additionResult: number = 5+3;
+console.log("additionResult:", additionResult);
+
+let subtractionResult: number = 10-2;
+console.log("subtractionResult:", subtractionResult);
+
+let multiplicationResult: number = 4*2;
+console.log("multiplicationResult:", multiplicationResult);
+
+let divisionResult: number = 16/2;
+console.log("divisionResult:", divisionResult);
