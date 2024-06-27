7feffc14a816e16361bde89156b5409e985d81d9
[hira  shaikh] <[hirasheikh556@gmail.com]>
2024-06-27 15:56:26 +0500

    first commit

---
+++ Assignment 9/main.js
@@ -0,0 +1,7 @@
+//assignment no 9
+//Favorite Number:
+// Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoritedNumber = 10;
+var messageResult = "My Favorite Number: ".concat(favoritedNumber);
+console.log(messageResult);
---
+++ Assignment 9/main.ts
@@ -0,0 +1,8 @@
+//assignment no 9
+//Favorite Number:
+// Store your favorite number in a variable. 
+//Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoritedNumber: number = 10;
+let messageResult: string =`My Favorite Number: ${favoritedNumber}`
+console.log (messageResult);
